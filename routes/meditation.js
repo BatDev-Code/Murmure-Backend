650e093e4054cdf0d4a3dc26f773432a53735fd3
Elisa Gomez <elysa3148@gmail.com>
2025-12-11 19:26:59 +0100

    Création base de données pour les meditations sur mongoDB, compris mise à jour des routes

--- routes/meditation.js
+++ routes/meditation.js
@@ -1,47 +1,57 @@
 const express = require("express");
 const router = express.Router();
-
+const Meditation = require("../models/meditations");
+const { checkBody } = require("../modules/checkBody");
 // Tableaux des url en fonction des choix theme, duration pour le mode "guidee" et éventuellement "solo" (si url musique ajouté plus tard)
-const meditations = [
-  {
-    theme: "anxiete",
-    mode: "guidee",
-    durations: {
-      3: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765279887/Murmure_Anxiete_3_min_abgfhl.mp3", //IA ElevenLabs
-      5: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
-      10: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
-    },
-  },
-  {
-    theme: "detente",
-    mode: "guidee",
-    durations: {
-      3: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a", //Stéphanie
-      5: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
-      10: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
-    },
-  },
-  {
-    theme: "sommeil",
-    mode: "guidee",
-    durations: {
-      3: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765279302/Murmure_Sommeil_3min_y34fz0.mp3", //IA ElevenLabs
-      5: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
-      10: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
-    },
-  },
-];
 
-router.post("/", async (req, res) => {
+// const meditations = [
+//   {
+//     theme: "anxiete",
+//     mode: "guidee",
+//     durations: {
+//       3: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765279887/Murmure_Anxiete_3_min_abgfhl.mp3", //IA ElevenLabs
+//       5: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
+//       10: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
+//     },
+//   },
+//   {
+//     theme: "detente",
+//     mode: "guidee",
+//     durations: {
+//       3: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a", //Stéphanie
+//       5: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
+//       10: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
+//     },
+//   },
+//   {
+//     theme: "sommeil",
+//     mode: "guidee",
+//     durations: {
+//       3: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765279302/Murmure_Sommeil_3min_y34fz0.mp3", //IA ElevenLabs
+//       5: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
+//       10: "https://res.cloudinary.com/dmbowwvt8/video/upload/v1765199945/Murmure__Medit_detente_3_min.m4a",
+//     },
+//   },
+// ];
+
+router.post("/player", async (req, res) => {
   try {
-    const { type, mode, duration } = req.body;
+    const { theme, mode, duration } = req.body;
 
-    console.log("POST /meditation :", type, mode, duration);
+    console.log("POST /meditation/player :", theme, mode, duration);
+
+    // Vérifier que toutes les données sont là
+    if (!checkBody(req.body, ["theme", "mode", "duration"])) {
+      return res.json({ result: false, error: "Missing or empty fields" });
+    }
 
     // Identifier la bonne méditation
-    const entry = meditations.find(
-      (item) => item.theme === type && item.mode === mode
-    );
+    const entry = await Meditation.findOne({
+      theme,
+      mode,
+      duration: Number(duration),
+    });
+    console.log("entry =", entry);
 
     if (!entry) {
       return res.status(404).json({
@@ -50,25 +60,35 @@ router.post("/", async (req, res) => {
       });
     }
 
-    // Récupérer l'url selon la durée
-    const audioUrl = entry.durations[duration];
-
-    if (!audioUrl) {
-      return res.status(404).json({
-        result: false,
-        error: "Durée non disponible pour cette méditation.",
-      });
-    }
-
+    
     // Réponse finale si tout se passe bien
     res.json({
       result: true,
-      audioUrl,
+      audioUrl: entry.audioUrl,
     });
   } catch (error) {
-    console.error("Erreur /meditation :", error);
+    console.error("Erreur /meditation/player :", error);
     res.status(500).json({ result: false, error: "Erreur serveur" });
   }
+});
+
+// *****************************************************
+// Module backend pour ajouter des méditations (mais non reporté sur le front) - Mode production
+
+router.post("/", (req, res) => {
+  const { theme, mode, duration, audioUrl, imageUrl } = req.body;
+
+  const newMeditation = new Meditation({
+    theme,
+    mode,
+    duration,
+    audioUrl,
+    imageUrl,
+  });
+  newMeditation.save().then((newMeditation) => {
+    console.log("New Meditation saved", newMeditation);
+    Meditation.find().then((allMeditations) => res.json({ allMeditations }));
+  });
 });
 
 module.exports = router;
